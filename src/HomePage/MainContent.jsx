4cd544703f5765844fd6e3caa1dec3d5201cf759
Dean Barrow <deanjordanbarrow@gmail.com>
2025-02-21 16:27:20 +1300

    Added images, title & short paragraph to MAINContent.jsx. Working on positiong them correctly in CSS

--- src/HomePage/MainContent.jsx
+++ src/HomePage/MainContent.jsx
@@ -5,13 +5,19 @@ function MainContent() {
       return (
             <div className={styles.mainContainer}>
                   <div className={styles.content1}>
-                        <h1>MainContent</h1>
+                        <img src="src\assets\MarketingStategies.jpg"></img>
+                        <h1>Marketing</h1>
+                        <p>Learn effective marheting strategies</p>
                   </div>
                   <div className={styles.content2}>
-                        <h1>MainContent2</h1>
+                        <img src="src\assets\TargetMarket.jpg"></img>
+                        <h1>Target Market</h1>
+                        <p>Everyone is not your customer, learn how to target the right customers</p>
                   </div>
                   <div className={styles.content3}>
-                        <h1>MainContent3</h1>
+                        <img src="src\assets\SocialMarketing.jpg"></img>
+                        <h1>Social Media Marketing</h1>
+                        <p>Create a social media presence with these marketing strategies</p>
                   </div>
             </div>
       );
